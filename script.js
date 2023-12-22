b769c109b155c6e3a11f4a085cf2b50bf040190c
Nildo Galvino <94250281+nildogalvino@users.noreply.github.com>
2023-12-22 07:36:02 -0300

    Curso JavaScript avançado do Matheus Fraga
    iniciando o curso avançado de Javascript...

---
+++ script.js
@@ -0,0 +1,19 @@
+// SeInterval
+
+function acao(){
+    document.write("Execultadando... <br/>")
+}
+
+//var timer = setInterval(() => {
+  //  document.write("Execultadando!!! <br/>");
+//}, 1000)
+
+
+
+
+
+// SetTimeout
+
+setTimeout(() =>{
+     console.log("EXECUTOU OSSO TIMEOUT!!");
+}, 3000);
