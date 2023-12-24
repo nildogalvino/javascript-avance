a712a542096075ea77981332244794e7edb194e2
Nildo Galvino <94250281+nildogalvino@users.noreply.github.com>
2023-12-24 10:41:55 -0300

    Curso JavaScript Matheus Fraga
    hoje a aula foi de objetos.

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 
+// Objetos
 
-// Diferenças entre VAR, LET, CONST
+//let pessoa = {
+//    nome: 'Nildo',
+//    idade: 35,
+//    altura: 1.77,
+//    prossiçao: "Programador FullStack",
+//};
+//
+//console.log(pessoa);
+//
+//let carro = {
+//    nome: 'Golf 1.6',
+//    cor: 'Branco',
+//    potencia: '140cv',
+//}
+//
+//console.log(carro);
+
+let usuarios = [
+    {nome: "Nildo", cargo: "Programador", status: "ATIVO"},
+    {nome: "Silvia", cargo: "Aposentada", status: "ATIVO"},
+    {nome: "Junior", cargo: "Autonomo", status: "ATIVO"},
+];
+
+console.log(usuarios);
