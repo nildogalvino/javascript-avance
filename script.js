41af0798305095bf6a4fe515232c33e51d21819e
Nildo Galvino <94250281+nildogalvino@users.noreply.github.com>
2023-12-24 05:50:47 -0300

    Curso Matheus Fraga
    Aula feita no console do chrome

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-// SeInterval
-
-function acao(){
-    document.write("Execultadando... <br/>")
-}
-
-//var timer = setInterval(() => {
-  //  document.write("Execultadando!!! <br/>");
-//}, 1000)
 
 
-
-
-
-// SetTimeout
-
-setTimeout(() =>{
-     console.log("EXECUTOU OSSO TIMEOUT!!");
-}, 3000);
+// Diferenças entre VAR, LET, CONST
