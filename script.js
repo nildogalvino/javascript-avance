dc5ecfb1f20d85c12d3b74f89938785c04154a56
Nildo Galvino <94250281+nildogalvino@users.noreply.github.com>
2023-12-26 09:23:13 -0300

    Curso Matheus Fraga
    Entendendo um pouco sobre desconstrução de objeto e array

--- script.js
+++ script.js
@@ -1,11 +1,42 @@
 
 
 
-// Template strings
+// Descontruir um Objeto
 
-let nome = 'Nildo';
-let sobrenome = 'Galvino';
-let idade = 35;
 
-let mensagem = "Meu nome é  "+ nome+" "+ sobrenome +" e eu tenho "+idade +" anos de idade"
-console.log(mensagem);
+let pessoa = {
+    nome: 'Nildo',
+    sobrenome: 'Galvino',
+    empresa: 'NG Tech',
+    cargo: 'Programador Fullstack',    
+};
+
+//console.log(pessoa.nome);
+//console.log(pessoa.cargo);
+
+//let nome = 'TESTE';
+//
+//const { nome:nomePessoa, cargo, empresa, sobrenome } = pessoa;
+//
+//console.log('nome da pessoa ' + nomePessoa);
+//console.log(sobrenome);
+//
+//
+//console.log(cargo);
+//console.log(empresa);
+
+
+
+// ===========================================================
+
+
+let nomes = ['Maria', 'Jose', 'Joao'];
+
+//let {0:Maria, 2:terceiraPessoa } = nomes;
+//console.log(Maria);
+//console.log(terceiraPessoa);
+
+let [primeironome, segundonome, terceironome] = nomes;
+console.log(primeironome);
+console.log(segundonome);
+console.log(terceironome);
